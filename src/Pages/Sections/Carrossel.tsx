db26628f076882d7412b3cac72d42557ca94b543
RalphSM <ralphsmateus@live.com>
2025-02-04 23:21:27 -0300

    5 commit

--- src/Pages/Sections/Carrossel.tsx
+++ src/Pages/Sections/Carrossel.tsx
@@ -1,12 +1,15 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import { register } from "swiper/element/bundle";
+register();
+import "swiper/css/scrollbar";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import img1 from "../../assets/Imagens/img-1.jpg";
-import img2 from "../../assets/Imagens/img-2.webp";
-import img3 from "../../assets/Imagens/img-3.webp";
-import img4 from "../../assets/Imagens/img-4.webp";
+import img1 from "../../assets/Imagens/academia1.jpg";
+import img2 from "../../assets/Imagens/academia2.jpg";
+import img3 from "../../assets/Imagens/academia3.jpg";
+import img4 from "../../assets/Imagens/academia4.jpg";
 
 const Carrossel = () => {
   const imagens = [img1, img2, img3, img4];
